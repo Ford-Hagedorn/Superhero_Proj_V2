2d6bf8e86872cca4d51222627abd52b5ebd41fe2
Fordley Hagedorn <fordleyh2900@gmail.com>
2021-03-16 15:56:09 -0500

    Made a new repository with gitignore

---
+++ FordSuperheroes/FordSuperheroes/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using FordSuperheroes.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FordSuperheroes.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Hero> Superhero { get; set; }
+
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ FordSuperheroes/FordSuperheroes/Models/Hero.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FordSuperheroes.Models
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string HeroName { get; set; }
+        public string HeroPowers { get; set; }
+        public string HeroSubPowers { get; set; }
+        public string HeroRealName { get; set; }
+        public string CatchPhrase { get; set; }
+
+    }
+}
